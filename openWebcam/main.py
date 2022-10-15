8889b419c135e9b1a2167e563f38ce3578b5b678
segin-GH <segin.bytes@gmail.com>
2022-10-15 17:26:02 +0530

    prg for launching a web cam

---
+++ openWebcam/main.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+def main():
+    
+    print(cv2.__version__)
+    cam = cv2.VideoCapture(2)
+    while True:
+        ignore, frame = cam.read()
+        greayframe = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('myWebcam',greayframe)
+        cv2.moveWindow('myWebcam',0,0)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    cam.release()
+
+
+
+
+if __name__ == "__main__":
+    main()
