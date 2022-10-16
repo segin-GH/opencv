824da1785290769f18ef4539b840503fba8ef019
segin-GH <segin.bytes@gmail.com>
2022-10-16 20:37:40 +0530

    .

--- openWebcam/main.py
+++ openWebcam/main.py
@@ -7,8 +7,8 @@ def main():
     cam = cv2.VideoCapture(2)
     while True:
         ignore, frame = cam.read()
-        greayframe = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        cv2.imshow('myWebcam',greayframe)
+        # greayframe = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow('myWebcam',frame)
         cv2.moveWindow('myWebcam',0,0)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
