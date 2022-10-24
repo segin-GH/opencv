ac796d65d2a45ef53c3b7bed377b62531a57881b
segin-GH <segin.bytes@gmail.com>
2022-10-24 11:32:03 +0530

    .

--- imgAsDataArray/main.py
+++ imgAsDataArray/main.py
@@ -3,17 +3,13 @@ import numpy as np
 
 def main():
     while True:
-        frame = np.zeros([400,400,3],dtype = np.uint8)  # type: ignore
+        frame = np.zeros([200,200,3],dtype = np.uint8)  # type: ignore
         frame[:,:] = [255,255,255]
-        for i in range(0,400,50):
+        for i in range(0,200,50):
             j = 50 + i
             k = 100 + i 
-            l = 150 + i
-            m = 200 + i
             frame[i:j,j:k] = [0,0,0]
             frame[j:k,i:j] = [0,0,0]
-            frame[:j,l:] = [0,0,0]
-            frame[l:,:j] = [0,0,0]
             cv2.imshow('MyWindow',frame)
 
         if cv2.waitKey(1) & 0xff == ord('q'):
