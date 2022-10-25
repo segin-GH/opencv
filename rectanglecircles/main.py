0453ceb94c08f23cd103581f07c55b41915c24da
segin-GH <segin.bytes@gmail.com>
2022-10-25 13:05:09 +0530

    .

--- rectanglecircles/main.py
+++ rectanglecircles/main.py
@@ -6,7 +6,7 @@ def main():
     
     print(cv2.__version__)
     width = 640
-    height = 360
+    height = 460
     cam = cv2.VideoCapture(2)
     cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
@@ -15,7 +15,7 @@ def main():
     
     while True:
         ignore, frame = cam.read()
-        cv2.rectangle(frame,(0,0),(0,100),(0,255,0),2)
+        cv2.rectangle(frame,(280,200),(400,300),(0,255,0),2)
         cv2.imshow('webcam',frame)
         cv2.moveWindow('webcam',0,0)
         if cv2.waitKey(1) & 0xff == ord('q'):
