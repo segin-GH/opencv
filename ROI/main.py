92a3f5cf44104cb26555917999d8268878ac047b
segin-GH <segin.bytes@gmail.com>
2023-04-12 22:52:38 +0530

    .

--- ROI/main.py
+++ ROI/main.py
@@ -1,30 +1,30 @@
 import cv2
 
 
-def main ():
+def main():
     print(cv2.__version__)
     width = 640
     height = 460
     cam = cv2.VideoCapture(2)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
-    cam.set(cv2.CAP_PROP_FPS,30)
-    cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
-    
-    while(True):
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    cam.set(cv2.CAP_PROP_FPS, 30)
+    cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+
+    while (True):
         ignore, frame = cam.read()
-        frameROI = frame[100:210,250:390]
-        frameROIGray = cv2.cvtColor(frameROI,cv2.COLOR_BGR2GRAY)
-        frameROIGray = cv2.cvtColor(frameROIGray,cv2.COLOR_GRAY2BGR)
-        frame[100:210,250:390] = frameROIGray
-        
-        cv2.imshow('webcam',frame)
-        cv2.moveWindow('webcam',0,0)
-        cv2.imshow('myROI',frameROI)
-        cv2.moveWindow('myROI',650,0)
-        cv2.imshow('myGrayROI',frameROIGray)
-        cv2.moveWindow('myGrayROI',650,300)
-        
+        frameROI = frame[100:210, 250:390]
+        frameROIGray = cv2.cvtColor(frameROI, cv2.COLOR_BGR2GRAY)
+        frameROIGray = cv2.cvtColor(frameROIGray, cv2.COLOR_GRAY2BGR)
+        frame[100:210, 250:390] = frameROIGray
+
+        cv2.imshow('webcam', frame)
+        cv2.moveWindow('webcam', 0, 0)
+        cv2.imshow('myROI', frameROI)
+        cv2.moveWindow('myROI', 650, 0)
+        cv2.imshow('myGrayROI', frameROIGray)
+        cv2.moveWindow('myGrayROI', 650, 300)
+
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
     cam.release()
